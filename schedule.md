2cdab496c655cbaf8db757c263327d9e21a5cb25
Jack <jackinpeace@gmail.com>
2014-06-10 18:31:54 +0800

    coupon color

--- schedule.md
+++ schedule.md
@@ -20,4 +20,4 @@ service-1.slow,2.toast,3.exit,4.confirmBtn
 coupon
 courier ic
 234,117,130/17,184,174/243,42,107
-
+F1A2AB,6EB9F2,60F67B,F4437B
--- src/com/lehuo/ui/adapter/msp/ListAdapterCoupon.java
+++ src/com/lehuo/ui/adapter/msp/ListAdapterCoupon.java
@@ -3,10 +3,13 @@
  */
 package com.lehuo.ui.adapter.msp;
 
+import java.util.Currency;
 import java.util.List;
 
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
+import com.lehuo.MyApplication;
 import com.lehuo.R;
 import com.lehuo.ui.custom.list.ListItemImpl;
 import com.lehuo.ui.custom.list.MspAdapter;
@@ -21,8 +24,8 @@ public class ListAdapterCoupon extends MspAdapter {
 	public ListAdapterCoupon() {
 		super();
 	}
-
-
+	final int[] BGS = new int[]{R.color.coupon_bg1,R.color.coupon_bg2,R.color.coupon_bg3,R.color.coupon_bg4};
+final static long a = System.currentTimeMillis()&10;//0610
 	public class CouponViewHolder extends ViewHolderImpl{
 
 		TextView tv_name,tv_desc,tv_timestart,tv_timeends;//tv_righttop;
@@ -34,7 +37,6 @@ public class ListAdapterCoupon extends MspAdapter {
 			tv_timestart= (TextView)getHolderView().findViewById(R.id.tv_itemcoupon_timestart);
 			tv_timeends= (TextView)getHolderView().findViewById(R.id.tv_itemcoupon_timeends);
 //			tv_righttop= (TextView)getHolderView().findViewById(R.id.tv_itemc);
-			
 		}
 
 		@Override
@@ -44,6 +46,13 @@ public class ListAdapterCoupon extends MspAdapter {
 			tv_desc.setText("µÖ¿Û½ð¶î"+itm.getType_money());//
 			tv_timestart.setText(itm.getUse_start_date());
 			tv_timeends.setText(itm.getUse_end_date());
+			try {
+				((FrameLayout) getHolderView()).getChildAt(0).setBackgroundColor(
+						MyApplication.app().getResources()
+								.getColor(BGS[((int)a+position) % BGS.length]));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 
 		@Override
